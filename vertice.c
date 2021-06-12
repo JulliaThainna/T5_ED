4e692d0bf1544512bb89c96a06b248f86ba529eb
Rafael Furlanetto <rafael.furlanetto@uel.br>
2021-06-11 23:18:19 -0300

    Correcting identation

--- vertice.c
+++ vertice.c
@@ -5,46 +5,39 @@
 #include "vertice.h"
 #include "point.h"
 
-typedef struct
-{
+typedef struct{
     char nome[100];
     Point point;
 } VerticeStruct;
 
-Vertice createVertice(char *nome, float x, float y)
-{
-    VerticeStruct *vert = (VerticeStruct *)malloc(sizeof(VerticeStruct));
+Vertice createVertice(char* nome, float x, float y){
+    VerticeStruct* vert = (VerticeStruct*)malloc(sizeof(VerticeStruct));
     strcpy(vert->nome, nome);
     vert->point = criaPoint(x, y);
     return vert;
 }
 
-char *verticeGetNome(Vertice vertice)
-{
-    VerticeStruct *vert = (VerticeStruct *)vertice;
+char *verticeGetNome(Vertice vertice){
+    VerticeStruct* vert = (VerticeStruct*)vertice;
     return vert->nome;
 }
 
-Point verticeGetPoint(Vertice vertice)
-{
-    VerticeStruct *vert = (VerticeStruct *)vertice;
+Point verticeGetPoint(Vertice vertice){
+    VerticeStruct* vert = (VerticeStruct*)vertice;
     return vert->point;
 }
 
-float verticeGetX(Vertice vertice)
-{
-    VerticeStruct *vert = (VerticeStruct *)vertice;
+float verticeGetX(Vertice vertice){
+    VerticeStruct* vert = (VerticeStruct*)vertice;
     return getPointX(vert->point);
 }
 
-float verticeGetY(Vertice vertice)
-{
-    VerticeStruct *vert = (VerticeStruct *)vertice;
+float verticeGetY(Vertice vertice){
+    VerticeStruct* vert = (VerticeStruct*)vertice;
     return getPointY(vert->point);
 }
 
-void desenhaVerticeSvg(Vertice vertice, FILE *fileSvg)
-{
+void desenhaVerticeSvg(Vertice vertice, FILE* fileSvg){
     fprintf(fileSvg, "\n\t<circle cx=\"%f\" cy=\"%f\" r=\"10\" stroke=\"black\" fill=\"white\" stroke-width=\"0.5\" />", verticeGetX(vertice), verticeGetY(vertice));
     fprintf(fileSvg, "\n\t<text x=\"%f\" y=\"%f\" text-anchor=\"middle\" font-size=\"0.5em\">%s</text>", verticeGetX(vertice), verticeGetY(vertice), verticeGetNome(vertice));
 }
